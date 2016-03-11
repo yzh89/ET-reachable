f81f136d3d2af2f51e34ae6abad246d981ae5e51
Yuchen Zhou <yzh89@umd.edu>
2016-03-11 11:04:13 -0500

    Add cvx part for control synthesis

--- find_time_varying_control_set.m
+++ find_time_varying_control_set.m
@@ -1,76 +1,86 @@
-function [q,alpha]= find_time_varying_control_set(x0,x0shMat,qB,MB,l,Ac,Bc,u0,U0)
-%clear;
-%load result
-T=4;
-% basisMat = [1 zeros(1,9); 0 1 zeros(1,8); 0 0 1 zeros(1,7)]';  % orthogonal basis of (x1, x2) subspace
-% psObj_A = rsObj_A.projection(basisMat);  % reach set projection
-% psObj_A = psObj_A.cut(T);
-% psObj_B = rsObj_B.projection(basisMat);  % reach set projection
-% [x0,x0shMat]=x0EllObj_A.double();
-%[xB,xB0shMat]=x0EllObj_B.double();
-%[ctrMat, ttVec] = rsObj_B.get_center();
-%xB=ctrMat(:,end);
+%function [q_c,Q_c]= find_time_varying_control_set(N,T,Ac,Bc,lstar,c_i,M_i,c_x0,M_x0,c_U,M_U)
+%n is number of states 
+%m is number of control inputs
+%N is number of time samples
+%T is Estimated Collision Time
+%lstar is n*N matrix 
+%mu is weighing vector of length N
+%compute Sampling Time
+[n,m] = size(Bc);
+mu = ones(N,1);
+dt = T/N;
+%size of Pmatrix
+Psize = [n,m];
+%Intializing  P(i)
+blockP  = cell(N);
+lpMat = cell(N,1);
 
-% assume lstar
-l=[0;1;0;zeros(7,1)];
-l=l/norm(l);
-
-%%
-% varying factor to obtain good bound on U
-%Q0=shMat;
-%q0=centVec';
-
-factor=2:0.4:4;
-for iter=1:length(factor)
-    Q=Q0/factor(iter);
-    
-    %%
-    %Compute int G(t,s) from s=0 to t. G(t,s)=e(t-s)A
-    % as well as int of sqrt(l'GBQB'G'l)
-    N=6; % estimated number of product need to take to find Gint
-    Gtemp=eye(size(Ac))*T;
-    for i=1:N
-        mAc=mpower(-Ac,i);
-        if isequal(mAc,zeros(size(Ac)))
-            break
-        end
-        Gtemp=Gtemp+T^(i+1)/factorial(i+1)*mAc;
-    end
-    Gint=expm(Ac*T)*Gtemp;
-    f= @(s) sqrt(l'*expm(Ac*(T-s))*Bc*Q*Bc'*(expm(Ac*(T-s)))'*l);
-    GGint=integral(f,0,T,'ArrayValued',1);
-    
-    
-    % define QCQP
-    n = 3;
-    %f = -2*( xB'*l - l'*expm(Ac*T)*x0 - GGint(1) - sqrt(l'*expm(Ac*T)*x0shMat*...
-    %    (expm(Ac*T))'*l ))*l'*Gint*Bc;
-    fc=( xB'*l - l'*expm(Ac*T)*x0 - GGint(1) - sqrt(l'*expm(Ac*T)*x0shMat*...
-        (expm(Ac*T))'*l ) );
-    %H = Bc'*Gint'*(l*l')*Gint*Bc;
-    
-    cvx_begin
-    variables q(n)
-    (q-q0)'*inv(Q)*(q-q0) <= 1
-    maximize(fc-l'*Gint*Bc*q)
-    cvx_end
-    
-    cvx_begin SDP
-    variable lambda
-    lambda > 0
-    [inv(Q0)-lambda*inv(Q), -inv(Q0)*q0+lambda*inv(Q)*q;
-        (-inv(Q0)*q0+lambda*inv(Q)*q)', q0'*inv(Q0)*q0-1-lambda*(q'*inv(Q)*q-1)]<=0
-    minimize(0)
-    cvx_end
-    if isnan(lambda)
-        %continous
-    else
-        
-        plot(ellipsoid(q,Q))
-        hold on
-        plot(ellipsoid(q0,Q0))
-        break;
-    end
+c_i{1}=[1;zeros(9,1)];
+M_i{1}=diag([0.01,0.01,0.01,0,0,0, zeros(1,4)]);
+for i=2:N
+    c_i{i} = [1-0.2*(i-1)*dt;zeros(9,1)];
+    M_i{i} = 1.05*M_i{i-1};
 end
-%%
-return
+
+rIntMat = zeros(N,N);
+for i=1:1:N
+    for j=1:1:N 
+        blockP{i,j} = zeros(Psize);
+    end
+    lpMat{i} = zeros(N,m);
+end
+%computing P(i) & lP & rInt + matrix equivalent
+for k=1:1:N
+    for i =1:1:k
+        rInt_f = @(s) sqrt((lstar(:,k)')*expm(Ac*(k*dt-s))*Bc*M_U*Bc'*(expm(Ac*(k*dt -s)))'*lstar(:,k));
+        rIntMat(k,i) = integral(rInt_f,(i-1)*dt,i*dt,'ArrayValued',1);
+        
+        blockP_f = @(s) expm(Ac*(k*dt-s))*Bc;
+        blockP{k,i}= integral(blockP_f,(i-1)*dt,i*dt,'ArrayValued',1);
+        
+        lpMat{k}(i,:)=lstar(:,k)'*blockP{k,i};
+    end
+    
+end
+
+
+
+% 
+% function [h_q] = Calculateh(k_hat,Q)
+%     h_q = 0;
+%     for l=1:1:k_hat
+%         h_q = h_q + lstar(:,k_hat)'*blockP{k_hat,l}*Q(:,l);
+%     end 
+% end
+% function [g_a] = Calculateg(k_hat,alpha_hat)
+%     g_a = 0;
+%     for l=1:1:k_hat
+%         r = @(s) (lstar(:,k_hat)')*expm(Ac*(k_hat*dt-s))*Bc*M_U*Bc'*(expm(Ac*(k_hat*dt -s))')*lstar(:,k_hat);
+%         g_a = g_a +alpha_hat(l)*integral(r,(l-1)*dt,l*dt,'ArrayValued',1);
+%     end 
+% end
+
+
+cvx_begin SDP
+cvx_quiet false
+cvx_precision high
+variable q(m,N)
+variable lambda(N,1)
+variable a(N,1)
+a>0.1
+for k =1:1:N
+    lambda(k) > 0
+    [1-lambda(k),    zeros(1,m),      (q(:,k)-c_U')';
+        zeros(m,1),  lambda(k)*eye(m), a(k)*sqrt(M_U);
+        q(:,k)-c_U', a(k)*sqrt(M_U),   M_U] >=0
+    
+    
+    -rIntMat(k,:)*a - sum(diag(lpMat{k}*q))-(lstar(:,k)')*c_i{k} ...
+        + sqrt((lstar(:,k)')*M_i{k}*lstar(:,k))-lstar(:,k)'*expm(Ac*k*dt)*c_x0 ...
+        - sqrt(lstar(:,k)'*expm(Ac*k*dt)*M_x0*expm(Ac*k*dt)*lstar(:,k)) >0
+end
+minimize(-mu'*a)
+cvx_end
+
+
+%end
